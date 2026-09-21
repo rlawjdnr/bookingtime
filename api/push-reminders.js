8421419050c621f7ce25179871eb74e028ced1ed
김정욱 <gimjeong-uk@gimjeong-ug-ui-MacBookAir.local>
2026-09-21 23:52:48 +0900

    Add period to push reminder body

--- api/push-reminders.js
+++ api/push-reminders.js
@@ -4,7 +4,7 @@ const DEFAULT_SUPABASE_URL = "https://ohwvtwywwjbwlkknwjxe.supabase.co";
 const KST_OFFSET_MS = 9 * 60 * 60 * 1000;
 const REMINDER_LOOKAHEAD_MINUTES = 100;
 const REMINDER_WINDOW_MINUTES = 60;
-const REMINDER_BODY = "약속된 일정이 다가와 안내드립니다. 편안한 마음으로 내원해주세요";
+const REMINDER_BODY = "약속된 일정이 다가와 안내드립니다. 편안한 마음으로 내원해주세요.";
 
 module.exports = async function handler(request, response) {
   if (!["GET", "POST"].includes(request.method)) {
